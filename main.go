28bd8fa67f87318c56a4a1f562c9e406e658d15e
jgsheppa <james.sheppard@avisio.ai>
2021-12-03 00:02:04 +0100

    Add faq and 404 pages

--- main.go
+++ main.go
@@ -17,10 +17,24 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact</h1><a href=\"bob@example.com\">bob@example.com</a>")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Frequently Asked Questions</h1><ul><li>What is this site?</li><li>Who are you?</li><li>Who do you think you are?</li></ul>")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>We couldn't find that page!</h1><a href=\"/\">Home</a>")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	// HandlerFunc converts notFound to the correct type
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	fmt.Println("Starting the development server on port 3000...")
 	http.ListenAndServe(":3000", r)
 }
