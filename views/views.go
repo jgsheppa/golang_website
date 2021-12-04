71f39700838c6d181037f53fb8d5b496cafe43c5
jgsheppa <james.sheppard@avisio.ai>
2021-12-04 02:04:07 +0100

    Add new footer and refine view package

--- views/views.go
+++ views/views.go
@@ -2,13 +2,16 @@ package views
 
 import (
 	"html/template"
+	"path/filepath"
+)
+
+var (
+	LayoutDir string = "views/layouts/"
+	TemplateExt string = ".gohtml"
 )
 
 func NewView(layout string, files ...string) *View {
-	files = append(files, 
-		"views/layouts/navbar.gohtml",
-		"views/layouts/footer.gohtml", 
-		"views/layouts/bootstrap.gohtml")
+	files = append(files, layoutFiles()...)
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -24,4 +27,14 @@ func NewView(layout string, files ...string) *View {
 type View struct {
 	Template *template.Template
 	Layout string
+}
+
+// Returns a slice of strings 
+// representing the layout files
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
