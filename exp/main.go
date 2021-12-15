3d12edb69000de0580bf02c621c576a4d511a1ee
jgsheppa <james.sheppard@avisio.ai>
2021-12-16 00:25:43 +0100

    Start work on user model

--- exp/main.go
+++ exp/main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
-	"time"
 
+	"github.com/jgsheppa/golang_website/models"
 	_ "github.com/lib/pq"
-	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
 const (
@@ -31,25 +27,25 @@ func main () {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable", 
 	host, port, user, dbname)
 
-	newLogger := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
-		logger.Config{
-			SlowThreshold:              time.Second,   // Slow SQL threshold
-			LogLevel:                   logger.Info, // Log level
-			IgnoreRecordNotFoundError: true,           // Ignore ErrRecordNotFound error for logger
-			Colorful:                  true,          // Disable color
-		},
-	)
+	// newLogger := logger.New(
+	// 	log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+	// 	logger.Config{
+	// 		SlowThreshold:              time.Second,   // Slow SQL threshold
+	// 		LogLevel:                   logger.Info, // Log level
+	// 		IgnoreRecordNotFoundError: true,           // Ignore ErrRecordNotFound error for logger
+	// 		Colorful:                  true,          // Disable color
+	// 	},
+	// )
 
-	db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{
-		Logger: newLogger,
-	})
+	us, err := models.NewUserService(psqlInfo)
 	if err != nil {
 		panic(err)
 	}
+	// us.DestructiveReset()
+	user, err := us.ByID(2)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("USER", user)	
 
-	db.AutoMigrate(&User{})
-	var users []User
-	db.Find(&users)
-	fmt.Println(users)
 }
